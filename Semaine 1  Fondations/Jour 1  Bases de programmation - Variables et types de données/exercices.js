feda6757a91e74c6621e2d8a172240f75d0dc04c
Keltoummalouki <keltoummalouki911@gmail.com>
2025-03-21 21:43:09 +0000

    Jour 1

---
+++ Semaine 1  Fondations/Jour 1  Bases de programmation - Variables et types de données/exercices.js
@@ -0,0 +1,62 @@
+// Faciles :
+
+    // 8. 
+        let nom = "Malouki";
+        console.log("mon nom est " + nom);
+
+    // 9.
+        const age = 18;
+        console.log("mon age est : " + age);
+
+    // 10. 
+        let estEtudiant = true;
+        console.log(estEtudiant);
+
+// Moyens : 
+
+    // 8.
+        let a = 7;
+        let b = 2;
+        let c = a + b;
+        console.log("a = " + a + ", b = " + b);
+        console.log("a + b = " + c);
+
+    // 9. 
+        let firstName = "Keltoum ";
+        let lastName = "Malouki";
+        let fullName = firstName + lastName;
+        console.log("My full name is : " + fullName);
+        
+    // 10.
+        let nbr1 = parseInt("10");
+        let nbr2 = Number("40");
+        let result = nbr1+nbr2;
+        // console.log(nbr1+nbr2);
+        console.log("nbr1 = " + nbr1 + " , nbr2 = " + nbr2 + " resultat est : " + result);
+
+    // 11.
+        const fruits = ['Apple', 'Orange', 'banana'];
+        console.log(fruits);
+
+// Difficiles :
+
+    // 8. 
+        const personne = {
+            firstName : "Keltoum",
+            lastName : "Malouki",
+            age : 18,
+            city : "Casablanca"
+        }
+        console.log("my first name is : " + personne.firstName);
+        console.log("my last name is : " + personne.lastName);
+        console.log("my age is : " + personne.age);
+        console.log("I live in : " + personne.city);
+    
+    // 9. 
+        let compteur = 5;
+        console.log(compteur);
+        compteur = 10;
+        console.log(compteur);
+        
+
+
